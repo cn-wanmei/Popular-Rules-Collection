e17cb4b831ae0d3bd7991814861b7b3f0adc2f1f
cn-wanmei <143895395+cn-wanmei@users.noreply.github.com>
2026-09-22 21:04:44 +0800

    refactor(v3): remove obsolete V1 runtime package

--- src/engine/runtime/__init__.py
+++
@@ -1,3 +0,0 @@
-"""V1 runtime cutover primitives."""
-from .v1_cutover import CutoverReport, V1RuntimeCutover, run_v1_cutover_gate
-__all__ = ["CutoverReport", "V1RuntimeCutover", "run_v1_cutover_gate"]
